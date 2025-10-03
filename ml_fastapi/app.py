a81d1f1ada4e7236b166dc637be0d8b177977084
Mridul Gupta <mridul22gupta@gmail.com>
2025-10-03 19:39:51 +0530

    Add files via upload

---
+++ ml_fastapi/app.py
@@ -0,0 +1,103 @@
+''' 
+POST WHEN CLIENT SEND DATA TO SERVER AND WANTS SERVER TO PROCESS / INFERENCE ON ML model
+
+endpont( /predict) From client side to request body input details of user 
+Validate data
+send features to ML
+OUTPUT OF ML in json 
+
+'''
+
+from fastapi import  FastAPI, HTTPException, Path, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, EmailStr, Field , field_validator, model_validator,computed_field
+from typing import List, Dict, Optional, Annotated, Literal
+import pickle
+import pandas as pd
+import json
+
+app=FastAPI()
+
+# Step 1: Import pkl model
+
+with open ('/Users/softsuave/Basics FastApi/ML with Fastapi/model.pkl','rb') as f:
+    model=pickle.load(f)
+
+
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
+
+
+class UserInput(BaseModel):
+    
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the user')]
+    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Height of the user')]
+    income_lpa: Annotated[float, Field(..., gt=0, description='Annual salary of the user in lpa')]
+    smoker: Annotated[bool, Field(..., description='Is user a smoker')]
+    city: Annotated[str, Field(..., description='The city that the user belongs to')]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
+    
+    @computed_field(return_type=float)
+    @property
+    def bmi(self) -> float:
+        return round(self.weight / (self.height / 100) ** 2, 2)
+    
+    @computed_field
+    @property
+    def lifestyle_risk(self)->str:
+        if self.smoker and self.bmi >30:
+            return "high"
+        elif self.smoker or self.bmi >27:
+            return "medium"
+        else:
+            return "low"
+        
+    @computed_field
+    @property
+    def age_group(self) -> str:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "adult"
+        elif self.age < 60:
+            return "middle_aged"
+        return "senior"
+    
+    @computed_field
+    @property
+    def city_tier(self) -> int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        else:
+            return 3
+    
+@app.post("/predict")
+def predict_premium(data: UserInput):
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+    
+    prediction = model.predict(input_df)[0]
+    
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
+
+''' 
+to add requirements pip freeze >  requirements 
+
+'''
